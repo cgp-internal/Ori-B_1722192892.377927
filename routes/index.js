c7cbc1faac1f4bad522b0efefdd6d6ee082bc460
oribrook <brook.ori@gmail.com>
2024-07-28 21:55:03 +0300

    Adding routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const helloWorldRouter = express.Router();
+
+helloWorldRouter.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+module.exports = { helloWorldRouter };
